11eabba061972f08adf6a0818bd5af668f89750f
unknown <tsundzumo@gmail.com>
2020-05-08 11:29:22 +0200

    pushing to master

---
+++ src/main/java/Loadtime/Loadtime.java
@@ -0,0 +1,37 @@
+package Loadtime;
+
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+public class Loadtime {
+	
+  String driverPath = "";
+  public String baseUrl = "{name_of_env}apps.telkom.co.za/{name_of_application}";
+  public WebDriver driver;
+  
+  @BeforeTest
+  public void launchBrowser() {
+      System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "Files" + File.separator + "chromedriver_win32" + File.separator + "chromedriver.exe");
+      driver = new ChromeDriver();
+  }	
+
+  @Test
+  public void urlLogin() {
+	  driver.get("https://www.google.com/");
+  }
+
+  @AfterTest
+  public void closeBrowser() {
+	  driver.quit();
+  }
+
+}
---
+++ src/main/java/Loadtime/NewTest.java
@@ -0,0 +1,38 @@
+package Loadtime;
+
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewTest {
+	
+  String driverPath = "";
+  public String baseUrl = "{name_of_env}apps.telkom.co.za/{name_of_application}";
+  public WebDriver driver;
+  
+  @BeforeTest
+  public void launchBrowser() {
+      System.setProperty("webdriver.chrome.driver", System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "Files" + File.separator + "chromedriver_win32" + File.separator + "chromedriver.exe");
+      driver = new ChromeDriver();
+      driver.get("www.google.com");
+  }	
+
+  @Test
+  public void urlLogin() {
+	  
+  }
+
+  @AfterTest
+  public void closeBrowser() {
+	  
+  }
+
+}
