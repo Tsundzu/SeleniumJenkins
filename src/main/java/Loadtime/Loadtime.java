efa3387defb8b344f8fb02c4a14c5e25e72e6afd
unknown <tsundzumo@gmail.com>
2020-05-26 11:53:03 +0200

    final working code

--- src/main/java/Loadtime/Loadtime.java
+++ src/main/java/Loadtime/Loadtime.java
@@ -23,10 +23,15 @@ import org.xml.sax.helpers.DefaultHandler;
 
 @Test
 public class Loadtime extends DefaultHandler {
+	
   static Capabilities chromeCapabilities = DesiredCapabilities.chrome();
   static Capabilities firefoxCapabilities = DesiredCapabilities.firefox();
+  
   static String URL = "http://ecdev01apps.telkom.co.za/access-manager";
-  //static String URL;
+  static int responsecode = 0;
+  
+  static RemoteWebDriver chrome = null;
+  static RemoteWebDriver firefox = null;
 
   
   //Response code method, returns an integer.
@@ -35,66 +40,9 @@ public class Loadtime extends DefaultHandler {
 	}	
   
   
-  /*@Test(enabled = false)
-  public static void setUrl(String url) {
-	  URL = url;
-  }
-  */
-  
-  public static String getUrl() {return URL;}
 
   public static void main() {
-	  
-   RemoteWebDriver chrome = null;
-   RemoteWebDriver firefox = null;
    
-   int responsecode = 0;
-
- //Reading xml file to return the url value from Jenkins
-   try {
-		  
-		  File f = new File("C://Program Files (x86)//Jenkins//jobs//TestNg//config.xml");
-		  SAXParserFactory fact = SAXParserFactory.newInstance();
-		  SAXParser parser = fact.newSAXParser();
-		  
-		  DefaultHandler handler =  new DefaultHandler() {
-			  boolean desc = false;
-			  public void startElement(String uri, String localName, String qName,
-					  Attributes attr) throws SAXException{
-				
-				if(qName.equals("description")) {
-					String value = attr.getValue(qName);
-					System.out.println("Description: " + value);
-					desc = true;
-				}
-					
-			  }
-			  
-			  public void endElement(String uri, String localName, String qName) {
-				  
-				  if(qName.equals("description")) {
-					  //System.out.println("Description: " + qName);
-					  desc = false;
-				  } 
-			  }
-			  
-			  
-			  public  void characters(char[] ch, int start, int length) throws SAXException
-			  {
-				  
-				  if(desc) {
-					  System.out.println("description is : " + new String(ch,start, length));
-					  desc = false;
-				  }
-				  
-			  }
-		  };
-		  
-		  parser.parse(f, handler);
-		
-	} catch (Exception e) {
-		// TODO: handle exception
-	}
    
    //Testing against chrome browser
 	try {
@@ -110,20 +58,81 @@ public class Loadtime extends DefaultHandler {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
+
+ //Reading xml file to return the url value from Jenkins
+   try {
+		  
+		  File f = new File("C://Program Files (x86)//Jenkins//jobs//TestNg//config.xml");
+		  SAXParserFactory fact = SAXParserFactory.newInstance();
+		  SAXParser parser = fact.newSAXParser();
+		  
+		  DefaultHandler handler =  new DefaultHandler() {
+			  boolean desc = false;
+			  public void startElement(String uri, String localName, String qName,
+					  Attributes attr) throws SAXException{
+				
+				if(qName.equals("description")) {
+					//String value = attr.getValue(qName);
+					desc = true;
+				}
+					
+			  }
+			  
+			  public void endElement(String uri, String localName, String qName) {
+				  
+				  if(qName.equals("description")) {
+					  desc = false;
+				  } 
+			  }
+			  
+			  
+			  public  void characters(char[] ch, int start, int length) throws SAXException
+			  {
+				  
+				  if(desc) {
+					  
+					  // run against chrome
+					  chrome.get(new String(ch,start, length));
+					  System.out.println(chrome.getTitle());
+					  
+					  // run against firefox
+					  firefox.get(new String(ch,start, length));
+					  System.out.println(firefox.getTitle());
+					  
+					  //get http response code
+					  responsecode = httpResponseCode(new String(ch,start, length));
+					  System.out.println("HTTP Code: " + responsecode);
+					  
+					  //close both browsers
+					  chrome.quit();
+					  firefox.quit();
+					  
+					  
+					  desc = false;
+				  }
+				  
+			  }
+		  };
+		  
+		  parser.parse(f, handler);
+		
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
 	
-    responsecode = httpResponseCode(URL);
 
     // run against chrome
-    chrome.get(URL);
-    System.out.println(chrome.getTitle());
-    System.out.println("HTTP Code: " + responsecode);
+    //chrome.get("https://www.google.com");
+    //System.out.println(chrome.getTitle());
+    //responsecode = httpResponseCode("https://www.google.com");
+    //System.out.println("HTTP Code: " + responsecode);
 
     // run against firefox
-    firefox.get(URL);
-    System.out.println(firefox.getTitle());
-    System.out.println("HTTP Code: " + responsecode + getUrl());
+   // firefox.get(URL);
+    //System.out.println(firefox.getTitle());
+    //System.out.println("HTTP Code: " + responsecode + getUrl());
   
-    chrome.quit();
-    firefox.quit();
+    //chrome.quit();
+   // firefox.quit();
   }
 }
